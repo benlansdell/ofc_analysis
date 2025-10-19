ca7aee20c2eb4a453c8ab57bd87e9f98dba913f1
benlansdell <blansdell@sequel.ae>
2025-10-18 20:55:36 -0600

    Move more stuff

---
+++ src/spatial_decoder/run_spatial_decoder.sh
@@ -0,0 +1,7 @@
+python ./spatial_decoder.py Control_Animal_1
+python ./spatial_decoder.py Control_Animal_10
+python ./spatial_decoder.py Control_Animal_M3
+python ./spatial_decoder.py Clonidine_Animal_F4
+python ./spatial_decoder.py Clonidine_Animal_83
+python ./spatial_decoder.py IDX_Animal_F3
+python ./spatial_decoder.py IDX_Animal_80
